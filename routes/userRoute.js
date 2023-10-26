7f064daec7bac253e5ccdade66dfa5a83d1a3e01
HaiNam1408 <haiinamm1408@gmail.com>
2023-10-26 13:01:13 +0700

    up code

---
+++ routes/userRoute.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const ctrl = require('../controllers/userControllers');
+const { verifyToken, verifyAdmin } = require('../middlewares/verify');
+
+router.get('/', ctrl.getAll);
+router.get('/filter', ctrl.filter);
+router.get('/search', ctrl.search);
+router.get('/:id', ctrl.getUserById);
+router.post('/create', ctrl.create);
+router.put('/:id', [verifyToken, verifyAdmin], ctrl.update);
+router.delete('/:id', [verifyToken, verifyAdmin], ctrl.delete);
+
+module.exports = router;
